729e031ae1520d5269d40ea6495a115fb8240d46
bmmukhridin <73002669+bmmukhridin@users.noreply.github.com>
2022-09-01 19:34:42 -0700

    added-new-componets

--- src/App.js
+++ src/App.js
@@ -1,43 +1,8 @@
-import "./categories.styles.scss";
+import "./components/category-componets/category-item-styles.scss";
+import Directory from "./components/Directory/directory";
 
-import CategoryItem from "./components/category-item";
 function App() {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
-  return (
-    <div className="categories-container">
-      {categories.map((category) => {
-
-       return <CategoryItem key={category.id} category={category} />;
-      })}
-    </div>
-  );
+  return <Directory />;
 }
 
 export default App;
