05000f5c549ba04bb37f3e8a88df81d24c90b1a0
bmmukhridin <73002669+bmmukhridin@users.noreply.github.com>
2022-09-03 12:16:42 -0700

    added-SignInAuth

---
+++ src/components/sign-in-form copy/sign-in-form.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+  signInAuthUserWithEmailAndPassword,
+} from "../utils/firebase/firebase";
+import FormInput from "../Form-input/form-input";
+import "./sign-in.style.scss";
+import Button from "../Button-component/Button";
+const defaultFormField = {
+  email: "",
+  password: "",
+};
+
+function SignInForm() {
+  const [formField, setFormField] = useState(defaultFormField);
+  const { email, password } = formField;
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormField({ ...formField, [name]: value });
+  };
+
+  const signInWithGoogle = async function () {
+    const { user } = await signInWithGooglePopup();
+    await createUserDocumentFromAuth(user);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await signInAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+    } catch (error) {}
+  };
+  return (
+    <div className="sign-up-container">
+      <h2>Already have an account?</h2>
+      <span>Sign up with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label="Email"
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
+
+        <FormInput
+          label="Password"
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
+        <div className="buttons-container">
+          <Button type="submit">Sign In</Button>
+          <Button buttonType="google" onClick={signInWithGoogle}>
+            Google Sign In
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default SignInForm;
