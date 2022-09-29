b7bbdb5c78b0244fabc3230cb45336444ab7c92e
Mukhriddin Bakhramov <bmmukhridin@icloud.com>
2022-09-29 05:31:19 -0700

    started-w/-Redux-thunk

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,11 +1,10 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import { persistReducer, persistStore } from "redux-persist";
-
 import storage from "redux-persist/lib/storage";
 //import { legacy_createStore as createStore} from 'redux'
-
+import thunk from "redux-thunk";
 import { rootReducer } from "./root-reducer";
-// import logger from 'redux-logger';
+import logger from "redux-logger";
 
 const loggerMiddleware = (store) => (next) => (action) => {
   if (!action.type) {
@@ -29,7 +28,10 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 ///root-reducer
-const middlewares = [loggerMiddleware];
+const middlewares = [
+  process.env.NODE_ENV !== "production" && logger,
+  thunk,
+].filter(Boolean);
 const composedEnhaancer = compose(applyMiddleware(...middlewares));
 
 export const store = createStore(
