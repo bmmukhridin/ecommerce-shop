2873d102fb463881fb4e2e6ed2232f44a84ffa1f
bmmukhridin <73002669+bmmukhridin@users.noreply.github.com>
2022-10-08 17:23:47 -0700

    started-w/-Redux-saga

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -2,22 +2,24 @@ import { compose, createStore, applyMiddleware } from "redux";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 //import { legacy_createStore as createStore} from 'redux'
-import thunk from "redux-thunk";
+//import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+import { rootSaga } from "./root-saga";
 import { rootReducer } from "./root-reducer";
 import logger from "redux-logger";
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-  console.log("type: ", action.type);
-  console.log("payload: ", action.payload);
-  console.log("currentState", store.getState());
+// const loggerMiddleware = (store) => (next) => (action) => {
+//   if (!action.type) {
+//     return next(action);
+//   }
+//   console.log("type: ", action.type);
+//   console.log("payload: ", action.payload);
+//   console.log("currentState", store.getState());
 
-  next(action);
+//   next(action);
 
-  console.log("next state: ", store.getState());
-};
+//   console.log("next state: ", store.getState());
+// };
 
 const persistConfig = {
   key: "root",
@@ -25,12 +27,14 @@ const persistConfig = {
   blacklist: ["user"],
 };
 
+const sagaMiddleware = createSagaMiddleware()
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 ///root-reducer
 const middlewares = [
   process.env.NODE_ENV !== "production" && logger,
-  thunk,
+  sagaMiddleware
 ].filter(Boolean);
 const composedEnhaancer = compose(applyMiddleware(...middlewares));
 
@@ -39,5 +43,5 @@ export const store = createStore(
   undefined,
   composedEnhaancer
 );
-
+sagaMiddleware.run(rootSaga)
 export const persistor = persistStore(store);
