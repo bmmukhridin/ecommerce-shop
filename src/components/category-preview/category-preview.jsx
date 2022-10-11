93cf0ce2da3d05bb89afbb0d385a9c13b305626f
bmmukhridin <73002669+bmmukhridin@users.noreply.github.com>
2022-10-11 09:45:48 -0700

    fixed-bug-W/-<Link to>title

--- src/components/category-preview/category-preview.jsx
+++ src/components/category-preview/category-preview.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import ProductCard from "../product.card/product.card.component";
 import "./category-preview.styles.scss";
 
@@ -6,7 +7,9 @@ function CategoryPrewiev({ title, products }) {
   return (
     <div className="category-preview-container">
       <h2>
-        <span className="title">{title.toUpperCase()}</span>
+        <Link to={title}>
+          <span className="title">{title.toUpperCase()}</span>
+        </Link>
       </h2>
       <div className="preview">
         {products
